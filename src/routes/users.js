ba3513c6bb3cccd1e4306da8d2f6cde9ce68b793
Henrique <henrixe80@gmail.com>
2024-12-24 19:16:49 -0400

    V 1.0.4: Adicionando as funcionalidades de editar e excluir usuarios. Faltou modificar pra service a funcao de editar e excluir

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,75 @@
 const express = require("express");
-
+const { Usuario } = require("../models");
+const bcrypt = require("bcrypt");
 const router = express.Router();
-const userService = require("../services/usuario")
+const userService = require("../services/usuario");
 
-router.get('/listar-usuarios', async (req, res) => {
-    try {
-        let users = await userService.getDadosUser()
-        res.status(200).json({ users : users });
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ error: error.message });
+router.get("/listar-usuarios", async (req, res) => {
+  try {
+    let users = await userService.getDadosUser();
+    res.status(200).json({ users: users });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await userService.getDadosUserById(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error("Erro ao buscar usuário:", error);
+    return res.status(500).json({ message: "Erro interno do servidor" });
+  }
+});
+
+router.put("/users/:id", async (req, res) => {
+  try {
+    const { idAdm, senhaAdm, username, email, tipo } = req.body; // Informações do 
+    const idEditar = req.params.id; // ID do usuário a ser atualizado
+
+    const userUpdate = await userService.updateUser(
+        idAdm, senhaAdm, username, email, tipo, idEditar
+    );
+    if (userUpdate == false) {
+      return res.status(401).json({ message: "Usuario não autorizado" });
     }
-})
+    console.log(userUpdate)
+    return res.status(200).json({ message: "Usuário atualizado com sucesso" });
+  } catch (error) {
+    console.error("Erro ao atualizar usuário:", error);
+    return res.status(500).json({ message: "Erro interno do servidor" });
+  }
+});
+
+router.delete('/users', async (req, res) => {
+    try {
+      const { idAdm, senhaAdm, idExcluir } = req.body;
+  
+      const admin = await Usuario.findOne({ where: { id: idAdm, tipo: 'adm' } });
+      if (!admin) {
+        return res.status(403).json({ message: 'Permissão negada. Usuário não é um administrador.' });
+      }
+      const senhaValida = await bcrypt.compare(senhaAdm, admin.senha);
+      if (!senhaValida) {
+        return res.status(401).json({ message: 'Senha de administrador incorreta.' });
+      }
+  
+      const user = await Usuario.findOne({ where: { id: idExcluir } });
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário a ser excluído não encontrado.' });
+      }
+  
+      await user.destroy();
+      return res.status(200).json({ message: `Usuário com ID ${idExcluir} excluído com sucesso.` });
+    } catch (error) {
+      console.error('Erro ao excluir usuário:', error);
+      return res.status(500).json({ message: 'Erro interno do servidor.' });
+    }
+  });
 
 module.exports = router;
--- src/services/usuario.js
+++ src/services/usuario.js
@@ -1,9 +1,10 @@
 const { Usuario } = require("../models");
+const bcrypt = require("bcrypt");
 
 async function getDadosUser() {
   try {
     const users = await Usuario.findAll({
-      attributes: { exclude: ['senha'] },
+      attributes: { exclude: ["senha"] },
     });
     return users;
   } catch (error) {
@@ -11,5 +12,44 @@ async function getDadosUser() {
     return null;
   }
 }
+async function getDadosUserById(id) {
+  try {
+    const user = await Usuario.findOne({
+      where: { id },
+      attributes: { exclude: ["senha"] },
+    });
 
-module.exports = { getDadosUser };
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    return user;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+async function updateUser(idAdm, senhaAdm, username, email, tipo,idEditar ) {
+  try {
+    const admin = await Usuario.findOne({ where: { id: idAdm, tipo: 'adm' } });
+    if (!admin) {
+      return false
+    }
+
+    const senhaValida = await bcrypt.compare(senhaAdm, admin.senha);
+    if (!senhaValida) {
+      return false
+    }
+
+    const user = await Usuario.findOne({ where: { id: idEditar } });
+    if (!user) {
+      return false
+    }
+    await user.update({ username, email, tipo });
+    return true;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+module.exports = { getDadosUser, getDadosUserById, updateUser };
