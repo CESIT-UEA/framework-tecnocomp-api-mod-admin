a148123c34309fea5a1d7d0a32b920ee784ad7a1
Henrique <henrixe80@gmail.com>
2025-03-28 16:47:38 -0400

    Adicionando retry para verificar se corrige o bug do mysql

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,7 +3,10 @@ const {Sequelize, DataTypes} = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  port: 3306
+  port: 3306,
+  retry: {
+    max: 5
+  }
 });
 
 module.exports = { sequelize }
