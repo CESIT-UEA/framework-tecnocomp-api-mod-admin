acea44ec8ca9a5cb4417f9ada88eaf2b972bbe45
KaueWallace <kauewall8@gmail.com>
2026-01-22 17:42:59 -0400

    teste

--- src/utils/validarToken.js
+++ src/utils/validarToken.js
@@ -1,5 +1,6 @@
 async function validarToken(idToken) {
   const response = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${idToken}`);
+  console.log('idToken ',idToken)
   const data = await response.json();
   return data;
 }
