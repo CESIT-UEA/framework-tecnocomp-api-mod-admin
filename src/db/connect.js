b50f486254a964fd6c9a9ab5b620bd98306c8eec
KaueWallace <kauewall8@gmail.com>
2026-01-23 10:34:23 -0400

    tentativa de correção utf8

--- src/db/connect.js
+++ src/db/connect.js
@@ -4,6 +4,16 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   host: process.env.DB_HOST,
   dialect: 'mysql',
   port: 3306,
+  dialectOptions: {
+    charset: 'utf8mb4',
+    dateStrings: true,
+    typeCast: true
+  },
+  define: {
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci',
+    timestamps: true
+  },
   retry: {
     max: 5
   }
